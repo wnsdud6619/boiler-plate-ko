231b68644a6cbc565b25b2dd53806edda372ffed
Jayce <pj.jykim@kalmate.net>
2023-05-15 14:35:26 +0900

    비밀 정보보 보호

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+//09:11
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extened: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://JunYoungKim:abcd1234@atlascluster.yyu65uz.mongodb.net/?retryWrites=true&w=majority', {})
+console.log('mongoURI = ' + config.mongoURI);
+mongoose.connect(config.mongoURI, {}) 
 .then(() => console.log('MongoDb Connnceted...'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! test'));
+
+app.get('/', (req, res) => res.send('Hello World! 수정 테스트'));
+
+app.post('/register', async (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body);
+
+    await user.save()
+    .then(() => {
+        res.status(200).json({
+            success: true,
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.json({
+            success: false,
+            err: err,
+        })
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const userSchema = mongoose.Schema({
         type: String,
     },
     tokenExp: {
-        type: Numner,
+        type: Number,
     }
 })
 
